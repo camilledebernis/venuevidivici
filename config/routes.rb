ea9e269ce6dfd3015dd916ff55c129119c8d3a9b
Rodrigo Simon <rodrigo.simon85@gmail.com>
2019-11-27 15:06:06 -0300

    bookings form working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :bookings, only: [:new, :create, :index, :show, :destroy]
-  resources :venues, only: [:new, :create, :index, :show, :destroy]
+  resources :venues, only: [:new, :create, :index, :show, :destroy] do
+    resources :bookings, only: [:create]
+  end
 
   #devise_for :bookings
   #devise_for :venues
